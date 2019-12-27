6dcbe85c13b4ef539bf3f0985ce70c0efb0d9190
chalupaul <me@chalupaul.com>
2019-12-27 12:11:08 -0600

    wip

--- rpcv/api/app.py
+++ rpcv/api/app.py
@@ -7,6 +7,10 @@ from flask_rebar import Rebar
 
 from typing import Any, Dict
 
+from rpcv.common import log
+
+logger = log.get_logger()
+
 rebar = Rebar()
 registry = rebar.create_handler_registry(prefix="/api")
 
@@ -17,6 +21,7 @@ def create_app() -> Flask:
     dot_env = DotEnv()
     env_path = Path(os.path.abspath(__file__)).parent
     stage = os.environ.get("STAGE", "dev")
+    logger("Initializing for stage", stage=stage)
     env_file = os.path.join(env_path, f".env.{stage}")
     dot_env.init_app(app, env_file=env_file, verbose_mode=True)
 
