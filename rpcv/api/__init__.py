19fd3fbbd30b9bbbba4efa05306904ddbd8f2807
chalupaul <me@chalupaul.com>
2019-12-26 20:55:28 -0600

    reworking some stuff after moving code around

--- rpcv/api/__init__.py
+++ rpcv/api/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.1.0"
 
 # Import controllers for flask_rebar
-from api.controllers import hypervisor, cluster  # noqa: F401
+from rpcv.api.controllers import hypervisor, cluster  # noqa: F401
 
-from api.app import run_app  # noqa: F401
+from rpcv.api.app import run_app  # noqa: F401
